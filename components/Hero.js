cfba42e40d0a0cd005cc49f7f0a8eef0f100542e
Scott Davis <scottdavisxx@gmail.com>
2022-04-17 08:51:24 -0700

    fix hero component, delete boilerplate next code from index.js

--- components/Hero.js
+++ components/Hero.js
@@ -1,6 +1,12 @@
 
 
-const Create = () => {
-
+const Hero = () => {
+  return (
+    <div class="hero">
+    <h1>Scott Davis</h1>
+    <h2>Lorem ipsum dolor sit amet, consectetur adipiscing.</h2>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras aliquet id venenatis, odio sed odio.</p>
+  </div>
+  )
 }
-export default Create;
+export default Hero;
