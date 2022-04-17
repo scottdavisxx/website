b75fda0855065ad90c44dd91d4222e61f1438a72
Scott Davis <scottdavisxx@gmail.com>
2022-04-17 09:10:05 -0700

    migrate skills with intro html into component, link on index.js

---
+++ components/SkillsWithIntro.js
@@ -0,0 +1,16 @@
+const SkillsWithIntro = () => {
+  return (
+    <div class="logo-grid-with-intro">
+      <h2>Other Skills</h2>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem nec dictumst neque, 
+        non consectetur vitae porttitor metus, ac. Bibendum eget malesuada maecenas volutpat 
+        elit. Malesuada gravida egestas lorem dictum parturient eleifend. Aliquet mollis semper 
+        risus, orci ut a libero. Eu turpis donec sodales egestas facilisi eget aliquet. Orci et 
+        curabitur proin.
+      </p>
+      <div class="logo-grid">    
+      </div>
+    </div>
+  )
+}
+export default SkillsWithIntro;
