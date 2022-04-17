5fe60a761e5b21244bf80aabd577ad2e0eea03ab
Scott Davis <scottdavisxx@gmail.com>
2022-04-17 09:03:59 -0700

    migrate three col card component, import css

---
+++ components/ThreeColCards.js
@@ -0,0 +1,12 @@
+import SkillCard from "./elements/SkillCard";
+
+const ThreeColCards = () => {
+  return (
+    <div class="three-column-cards">
+      <SkillCard></SkillCard>
+      <SkillCard></SkillCard>
+      <SkillCard></SkillCard>
+    </div>
+  )
+}
+export default ThreeColCards;
--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import Hero from '../components/Hero'
+import ThreeColCards from '../components/ThreeColCards.js'
 
 export default function Home() {
   return (
@@ -12,6 +13,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Hero></Hero>
+      <ThreeColCards></ThreeColCards>
 
       <footer className={styles.footer}>
         
