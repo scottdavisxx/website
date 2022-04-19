aa5d56caef3a9c3b1e7f92482c1d87a3c886ca7a
Scott Davis <scottdavisxx@gmail.com>
2022-04-18 22:46:59 -0700

    update styles to tailwind

--- components/Hero.js
+++ components/Hero.js
@@ -3,9 +3,9 @@
 const Hero = () => {
   return (
     <div className="flex flex-col bg-prim-light justify-center py-5">
-    <h1 className="ml-16 mt-16 font-hero text-9xl text-comp-lighter w-3/6 font-bold leading-none">Scott Davis</h1>
-    <h2 className="text-comp-lighter text-center text-5xl my-4 font-bold">Lorem ipsum dolor sit amet, consectetur adipiscing.</h2>
-    <p className="text-comp-lighter text-center text-xl my-4 font-semibold">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras aliquet id venenatis, odio sed odio.</p>
+    <h1 className="ml-3 sm:ml-16 sm:mt-16 font-hero sm:text-9xl text-8xl  text-comp-lighter font-bold leading-none">Scott Davis</h1>
+    <h2 className="text-comp-lighter text-center text-5xl my-4 font-bold leading-none">Lorem ipsum dolor sit amet, consectetur adipiscing.</h2>
+    <p className="w-4/5 self-center text-comp-lighter text-center text-xl my-4 font-semibold">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras aliquet id venenatis, odio sed odio.</p>
   </div>
   )
 }
