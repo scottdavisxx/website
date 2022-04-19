19dec56070a93e9afbf61bcff7614f634978bdc4
Scott Davis <scottdavisxx@gmail.com>
2022-04-18 22:13:50 -0700

    fix margins on skill with intro

--- components/SkillsWithIntro.js
+++ components/SkillsWithIntro.js
@@ -4,7 +4,7 @@ const SkillsWithIntro = () => {
   return (
     <div className="bg-prim-darker flex flex-col items-center justify-between">
       <h2 className="text-white text-5xl font-bold my-5">Other Skills</h2>
-      <p className="text-white w-5/6 text-center my-2 text-2xl">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem nec dictumst neque, 
+      <p className="text-white w-5/6 text-center mb-5 text-2xl">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem nec dictumst neque, 
         non consectetur vitae porttitor metus, ac. Bibendum eget malesuada maecenas volutpat 
         elit. Malesuada gravida egestas lorem dictum parturient eleifend. Aliquet mollis semper 
         risus, orci ut a libero. Eu turpis donec sodales egestas facilisi eget.
