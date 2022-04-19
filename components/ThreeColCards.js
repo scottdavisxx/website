0b053c83b13411a01168d663c660ee5e0990c84a
Scott Davis <scottdavisxx@gmail.com>
2022-04-18 20:41:57 -0700

    style updates

--- components/ThreeColCards.js
+++ components/ThreeColCards.js
@@ -2,7 +2,7 @@ import SkillCard from "./elements/SkillCard";
 
 const ThreeColCards = () => {
   return (
-    <div className="bg-prim flex flex-row justify-center justify-evenly flex-wrap">
+    <div className="bg-prim flex flex-row justify-center justify-evenly flex-wrap py-5">
       <SkillCard></SkillCard>
       <SkillCard></SkillCard>
       <SkillCard></SkillCard>
