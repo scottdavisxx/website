426bdd89c1d567b4730acba24210abcc00aa4184
Scott Davis <scottdavisxx@gmail.com>
2022-04-18 22:57:55 -0700

    continue converting styles to tailwind

--- components/SkillsWithIntro.js
+++ components/SkillsWithIntro.js
@@ -2,7 +2,7 @@ import SkillIcon from "./elements/SkillIcon";
 
 const SkillsWithIntro = () => {
   return (
-    <div className="bg-prim-darker flex flex-col items-center justify-between">
+    <div className="bg-prim-darker flex flex-col items-center justify-evenly">
       <h2 className="text-white text-5xl font-bold my-5">Other Skills</h2>
       <p className="text-white w-5/6 text-center mb-5 text-2xl">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem nec dictumst neque, 
         non consectetur vitae porttitor metus, ac. Bibendum eget malesuada maecenas volutpat 
