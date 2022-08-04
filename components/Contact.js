df23bcd95241beba62edee6c554635545ec0efa3
Scott Davis <scottdavisxx@gmail.com>
2022-08-03 21:48:44 -0600

    style updates to contact, submit and inputs

--- components/Contact.js
+++ components/Contact.js
@@ -1,13 +1,13 @@
 const Contact = () => {
   return (
-    <div className="bg-prim-light flex flex-col align-center">
-      <h5 className="text-center text-prim-darker text-8xl">Contact Me</h5>
+    <div className="bg-prim-lighter flex flex-col align-center">
+      <h5 className="text-center text-prim-darker text-8xl font-extrabold">Contact Me</h5>
       <form className="flex justify-center flex-col self-center w-4/5 items-center">
         <label className="text-prim-darker self-start" htmlFor="name">
           Name:
         </label>
         <input
-          className="w-full mb-5"
+          className="w-full mb-5 bg-prim-light"
           type="text"
           id="name"
           name="name"
@@ -18,7 +18,7 @@ const Contact = () => {
           Email:
         </label>
         <input
-          className="w-full mb-5"
+          className="w-full mb-5 bg-prim-light"
           type="email"
           id="email"
           name="email"
@@ -29,7 +29,7 @@ const Contact = () => {
           Subject:
         </label>
         <input
-          className="w-full mb-5"
+          className="w-full mb-5 bg-prim-light"
           type="text"
           id="subject"
           name="subject"
@@ -38,10 +38,10 @@ const Contact = () => {
         <label className="text-prim-darker self-start" htmlFor="message">
           Message:
         </label>
-        <textarea className="w-full mb-5" type="text" id="message"></textarea>
+        <textarea className="w-full mb-5 bg-prim-light" type="text" id="message"></textarea>
 
         <input
-          className="w-full mb-5"
+          className="w-1/6 mb-5 bg-comp-light text-2xl rounded-lg py-1"
           type="submit"
           id="submit"
           value="Submit"
