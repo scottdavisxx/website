6dae42a6aa0159c51a83c68547825986228117cc
Scott Davis <scottdavisxx@gmail.com>
2022-08-03 21:42:24 -0600

    style updates to contact

--- components/Contact.js
+++ components/Contact.js
@@ -3,19 +3,49 @@ const Contact = () => {
     <div className="bg-prim-light flex flex-col align-center">
       <h5 className="text-center text-prim-darker text-8xl">Contact Me</h5>
       <form className="flex justify-center flex-col self-center w-4/5 items-center">
-        <label className="text-prim-darker" htmlFor="name">Name:</label>
-        <input className="w-4/5" type="text" id="name" name="name" defaultValue={""}></input>
+        <label className="text-prim-darker self-start" htmlFor="name">
+          Name:
+        </label>
+        <input
+          className="w-full mb-5"
+          type="text"
+          id="name"
+          name="name"
+          defaultValue={""}
+        ></input>
 
-        <label className="text-prim-darker" htmlFor="email">Email:</label>
-        <input className="w-4/5" type="email" id="email" name="email" defaultValue={""}></input>
+        <label className="text-prim-darker self-start" htmlFor="email">
+          Email:
+        </label>
+        <input
+          className="w-full mb-5"
+          type="email"
+          id="email"
+          name="email"
+          defaultValue={""}
+        ></input>
 
-        <label className="text-prim-darker" htmlFor="subject">Subject:</label>
-        <input className="w-4/5" type="text" id="subject" name="subject"></input>
+        <label className="text-prim-darker self-start" htmlFor="subject">
+          Subject:
+        </label>
+        <input
+          className="w-full mb-5"
+          type="text"
+          id="subject"
+          name="subject"
+        ></input>
 
-        <label className="text-prim-darker" htmlFor="message">Message:</label>
-        <textarea className="w-4/5" type="text" id="message"></textarea>
+        <label className="text-prim-darker self-start" htmlFor="message">
+          Message:
+        </label>
+        <textarea className="w-full mb-5" type="text" id="message"></textarea>
 
-        <input className="w-4/5" type="submit" id="submit" value="Submit"></input>
+        <input
+          className="w-full mb-5"
+          type="submit"
+          id="submit"
+          value="Submit"
+        ></input>
       </form>
     </div>
   );
