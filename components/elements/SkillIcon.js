13bf325451586419de3956d016dc9e3c1ff7d483
Scott Davis <scottdavisxx@gmail.com>
2022-08-07 22:42:06 -0600

    npm run format

--- components/elements/SkillIcon.js
+++ components/elements/SkillIcon.js
@@ -3,12 +3,7 @@ const SkillIcon = () => {
   return (
     <div className="flex flex-col my-5 w-39">
       <h4 className="text-prim-lighter text-4xl font-mono text-center">Git</h4>
-      <Image
-                alt="react logo"
-                src="/git.svg"
-                height="100"
-                width="100"
-      ></Image>
+      <Image alt="react logo" src="/git.svg" height="100" width="100"></Image>
     </div>
   );
 };
