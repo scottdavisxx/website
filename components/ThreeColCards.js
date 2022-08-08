85e1779311345c7cc104dbc69543866dc5defe0a
Scott Davis <scottdavisxx@gmail.com>
2022-08-07 20:02:23 -0600

    remove duplicate style from threeColCards

--- components/ThreeColCards.js
+++ components/ThreeColCards.js
@@ -2,7 +2,7 @@ import SkillCard from "./elements/SkillCard";
 
 const ThreeColCards = () => {
   return (
-    <div className="bg-prim flex flex-row justify-center justify-evenly flex-wrap py-5">
+    <div className="bg-prim flex flex-row justify-center flex-wrap py-5">
       <SkillCard></SkillCard>
       <SkillCard></SkillCard>
       <SkillCard></SkillCard>
---
+++ data/card.js
@@ -0,0 +1,14 @@
+export const card [
+  {
+    id: 1,
+    skill: 'React',
+  },
+  {
+    id: 2,
+    skill: 'Javascript',
+  },
+  {
+    id: 3, 
+    skill: 'Temp',
+  },
+]
