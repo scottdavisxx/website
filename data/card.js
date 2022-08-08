cc2111ea9fb3189b7cbbf3fb6a4ee49c66727aa5
Scott Davis <scottdavisxx@gmail.com>
2022-08-07 20:39:50 -0600

    populate data for cards

--- data/card.js
+++ data/card.js
@@ -1,14 +1,22 @@
-export const card [
+export const card = [
   {
     id: 1,
     skill: 'React',
+    image: '/react.svg',
+    blurb: '1 Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium explicabo repellat non alias facilis ipsam nam dicta ipsum laboriosam est, quo veritatis id aliquam quibusdam.',
   },
   {
     id: 2,
     skill: 'Javascript',
+    image: '/suit.svg',
+    blurb: '2 Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium explicabo repellat non alias facilis ipsam nam dicta ipsum laboriosam est, quo veritatis id aliquam quibusdam.',
+
   },
   {
     id: 3, 
     skill: 'Temp',
+    image: '/js.svg',
+    blurb: '3 Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium explicabo repellat non alias facilis ipsam nam dicta ipsum laboriosam est, quo veritatis id aliquam quibusdam.',
+
   },
 ]
