71ab8c65cefcd7883fd627152e6071329c6fe82e
Scott Davis <scottdavisxx@gmail.com>
2022-08-07 22:33:46 -0600

    made card data load from object file

--- components/ThreeColCards.js
+++ components/ThreeColCards.js
@@ -1,11 +1,30 @@
-import SkillCard from "./elements/SkillCard";
+import Image from "next/image";
+import { card } from "../data/cards";
 
 const ThreeColCards = () => {
   return (
     <div className="bg-prim flex flex-row justify-center flex-wrap py-5">
-      <SkillCard></SkillCard>
-      <SkillCard></SkillCard>
-      <SkillCard></SkillCard>
+      {card.map((data, key) => {
+        return (
+          <div
+            key={data.id}
+            className="border-comp border-2 rounded-xl flex flex-col w-96 my-5 mx-1 px-4 pb-4 border-box"
+          >
+            <div className="flex self-center my-6">
+              <Image
+                alt="react logo"
+                src={`/${data.image}`}
+                height="200"
+                width="200"
+              ></Image>
+            </div>
+            <div className="mt-2">
+              <h3 className="text-white text-4xl">{data.skill}</h3>
+              <p className="text-white text-2xl leading-tight">{data.blurb}</p>
+            </div>
+          </div>
+        );
+      })}
     </div>
   );
 };
---
+++ data/cards.js
@@ -0,0 +1,20 @@
+export const card = [
+  {
+    id: 1,
+    skill: "React",
+    image: "./react.svg",
+    blurb: "Pellentesque habitant morbi tristique senectus et netus. Quo usque tandem abutere, Catilina, patientia nostra? Paullum deliquit, ponderibus modulisque suis ratio utitur.",
+  },
+  {
+    id: 2,
+    skill: "JavaScript",
+    image: "./js.svg",
+    blurb: "Phasellus laoreet lorem vel dolor tempus vehicula. Unam incolunt Belgae, aliam Aquitani, tertiam. Nec dubitamus multa iter quae et nos invenerat. Gallia est omnis divisa in par.",
+  },
+  {
+    id: 3,
+    skill: "Professional",
+    image: "./suit.svg",
+    blurb: "Magna pars studiorum, prodita quaerimus. Lorem ipsum dolor sit amet, consectetur adipisici elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua.",
+  },
+];
