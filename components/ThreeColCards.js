3355f0bba617fbee71691717a5c0cf92643fc84a
Scott Davis <scottdavisxx@gmail.com>
2022-08-07 22:35:39 -0600

    delete unused key value

--- components/ThreeColCards.js
+++ components/ThreeColCards.js
@@ -4,23 +4,23 @@ import { card } from "../data/cards";
 const ThreeColCards = () => {
   return (
     <div className="bg-prim flex flex-row justify-center flex-wrap py-5">
-      {card.map((data, key) => {
+      {card.map((skillCard) => {
         return (
           <div
-            key={data.id}
+            key={skillCard.id}
             className="border-comp border-2 rounded-xl flex flex-col w-96 my-5 mx-1 px-4 pb-4 border-box"
           >
             <div className="flex self-center my-6">
               <Image
                 alt="react logo"
-                src={`/${data.image}`}
+                src={`/${skillCard.image}`}
                 height="200"
                 width="200"
               ></Image>
             </div>
             <div className="mt-2">
-              <h3 className="text-white text-4xl">{data.skill}</h3>
-              <p className="text-white text-2xl leading-tight">{data.blurb}</p>
+              <h3 className="text-white text-4xl">{skillCard.skill}</h3>
+              <p className="text-white text-2xl leading-tight">{skillCard.blurb}</p>
             </div>
           </div>
         );
