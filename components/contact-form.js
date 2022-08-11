5fc704730c825c4a57cf422a14c5d196ce7e1455
Scott Davis <scottdavisxx@gmail.com>
2022-08-10 23:25:52 -0600

    adjust style for form

--- components/contact-form.js
+++ components/contact-form.js
@@ -1,37 +1,77 @@
-import { useForm, ValidationError } from '@formspree/react';
+import { useForm, ValidationError } from "@formspree/react";
 function ContactForm() {
   const [state, handleSubmit] = useForm("moqbqvkl");
   if (state.succeeded) {
-      return <p>Thanks for joining!</p>;
+    return (
+    <div className="h-48 bg-comp">
+      <h3 className="text-8xl text-center text-prim align-middle">Thanks!</h3>;
+    </div>
+    )
   }
   return (
-      <form onSubmit={handleSubmit}>
-      <label htmlFor="email">
-        Email Address
-      </label>
-      <input
-        id="email"
-        type="email" 
-        name="email"
-      />
-      <ValidationError 
-        prefix="Email" 
-        field="email"
-        errors={state.errors}
-      />
-      <textarea
-        id="message"
-        name="message"
-      />
-      <ValidationError 
-        prefix="Message" 
-        field="message"
-        errors={state.errors}
-      />
-      <button type="submit" disabled={state.submitting}>
-        Submit
-      </button>
-    </form>
+    <div className="bg-prim-lighter flex flex-col align-center">
+      <h5 className="mt-3 text-center text-prim-darker text-5xl xl:text-8xl font-extrabold leading-none">
+        Contact Me
+      </h5>
+      <form
+        onSubmit={handleSubmit}
+        className="flex justify-center flex-col self-center w-4/5 items-center"
+      >
+        <label className="text-prim-darker self-start" htmlFor="name">
+          Name
+        </label>
+        <input
+          className="w-full mb-5 bg-prim-light"
+          type="text"
+          id="name"
+          name="name"
+          defaultValue={""}
+        ></input>
+        <label className="text-prim-darker self-start" htmlFor="email">
+          Email:
+        </label>
+        <input
+          className="w-full mb-5 bg-prim-light"
+          type="email"
+          id="email"
+          name="email"
+          defaultValue={""}
+        ></input>
+        <label className="text-prim-darker self-start" htmlFor="subject">
+          Subject:
+        </label>
+        <input
+          className="w-full mb-5 bg-prim-light"
+          type="text"
+          id="subject"
+          name="subject"
+        ></input>
+
+        <label className="text-prim-darker self-start" htmlFor="message">
+          Message:
+        </label>
+        <textarea
+          id="message"
+          name="message"
+          className="w-full mb-5 bg-prim-light"
+          type="text"
+        ></textarea>
+
+        <button
+          type="submit"
+          disabled={state.submitting}
+          className="w-fit mb-5 bg-comp-light text-2xl rounded-lg px-3 py-1"
+        >
+          Submit
+        </button>
+        <ValidationError prefix="Email" field="email" errors={state.errors} />
+        <ValidationError
+          prefix="Message"
+          field="message"
+          errors={state.errors}
+        />
+      </form>
+    </div>
   );
 }
 export default ContactForm;
