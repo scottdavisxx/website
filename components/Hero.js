a47124e7202809ebb98580bf785999b1df765839
Scott Davis <scottdavisxx@gmail.com>
2022-08-13 12:25:58 -0600

    adjust hero styles to fix white space on mobile and account for top nav

--- components/Hero.js
+++ components/Hero.js
@@ -1,10 +1,10 @@
 const Hero = () => {
   return (
-    <div className="flex flex-col bg-prim-light justify-center py-5">
-      <h1 className="ml-3 sm:ml-16 sm:mt-16 font-hero sm:text-9xl text-8xl  text-comp-lighter font-bold leading-none">
+    <div className="flex flex-col bg-prim-light justify-center pt-16 xl:pt-5 pb-5">
+      <h1 className="ml-3 sm:ml-16 sm:mt-16 font-hero text-8xl xl:text-9xl text-comp-lighter font-bold leading-none">
         Scott Davis
       </h1>
-      <h2 className="text-comp-lighter text-center text-5xl my-4 font-bold leading-none">
+      <h2 className="text-comp-lighter text-center text-4xl my-4 font-bold leading-none mt-6">
         Lorem ipsum dolor sit amet, consectetur adipiscing.
       </h2>
       <p className="w-4/5 self-center text-comp-lighter text-center text-xl my-4 font-semibold">
--- tailwind.config.js
+++ tailwind.config.js
@@ -41,7 +41,7 @@ module.exports = {
       '5xl': '3rem',
       '6xl': '4rem',
       '7xl': '5rem',
-      '8xl': '7rem',
+      '8xl': '6rem',
       '9xl':'14rem'
     },
   },
