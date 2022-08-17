e7871b7cd2cb9bc4a2e48d597fef7b983efabb3b
Scott Davis <scottdavisxx@gmail.com>
2022-08-16 22:31:47 -0600

    add new page for player, wrap index.js in <> instead of <div>

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,7 @@ import { Navbar } from '../components/Navbar'
 
 export default function Home() {
   return (
-    <div>
+    <>
        <Head>
         <title>Scott Davis</title>
       </Head>
@@ -20,6 +20,6 @@ export default function Home() {
       <SkillsWithIntro id="more-skills" />
       <Contact id="contact" />
       <HomeFooter id="social" />
-    </div>
+    </>
   )
 }
---
+++ pages/player.js
@@ -0,0 +1,14 @@
+import Head from "next/head";
+import { Navbar } from "../components/Navbar";
+import Hero from "../components/Hero";
+
+export default function Player() {
+  return (
+    <>
+      <Head>
+        <title>Scott Davis</title>
+      </Head>
+      <Navbar />
+    </>
+  )
+}
