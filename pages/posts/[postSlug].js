6da91d8b0b521453dc9782313b185112dcb20893
Scott Davis <scottdavisxx@gmail.com>
2022-08-20 22:26:45 -0600

    add featuredImage to query on post slugs, print image at top of blogs

--- pages/posts/[postSlug].js
+++ pages/posts/[postSlug].js
@@ -6,9 +6,9 @@ import { getApolloClient } from '../../lib/apollo-client';
 import { Navbar } from '../../components/Navbar';
 
 export default function Post({ post, site }) {
+  console.log(post, 'post');
   return (
-  
-    <div className="flex">
+    <div className="flex pb-8">
       <Head>
         <title>{ post.title }</title>
         <meta name="description" content={`Read more about ${post.title} on ${site.title}`} />
@@ -17,21 +17,30 @@ export default function Post({ post, site }) {
       <Navbar />
         <div className="bg-white mt-8 pt-8 flex flex-col xl:px-12">
           <div className="flex flex-col xl:mx-12 mx-4">
+          <div className="text-lg flex">
+          <Link href="/posts">
+            <a className='flex'>
+            <Image src="/back-arrow.svg" layout='intrinsic' width={15} height={15} alt='back arrow' />
+              <span className='px-1'>Back</span>
+            </a>
+          </Link>
+        </div>
             <h1 className="text-4xl text-comp my-1">
               { post.title }
             </h1>
+            <Image src={post.featuredImage.node.mediaItemUrl} layout='responsive' width={100} height={65} alt={post.title} />
             <div className="" dangerouslySetInnerHTML={{
               __html: post.content
             }} />
             </div>
-        <p className="text-lg flex">
+        <div className="text-lg flex">
           <Link href="/posts">
-            <a>
-            <Image src="/back-arrow.svg" layout='intrinsic' width={20} height={20} alt='back arrow' />
-              Blogs
+            <a className='flex'>
+            <Image src="/back-arrow.svg" layout='intrinsic' width={15} height={15} alt='back arrow' />
+              <span className='px-1'>Blogs</span>
             </a>
           </Link>
-        </p>
+        </div>
       </div>
     </div>
   )
@@ -53,6 +62,11 @@ export async function getStaticProps({ params = {} } = {}) {
           content
           title
           slug
+          featuredImage {
+            node {
+              mediaItemUrl
+            }
+          }
         }
       }
     `,
