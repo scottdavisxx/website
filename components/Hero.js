fcd4b4a43db1f796f49dd41fed019000135db49c
Scott Davis <scottdavisxx@gmail.com>
2023-09-24 18:51:31 -0600

    remove unused packages, escape apostrophe

--- components/Hero.js
+++ components/Hero.js
@@ -5,7 +5,7 @@ const Hero = () => {
         Scott Davis
       </h1>
       <h2 className="text-comp-lighter text-center text-4xl my-4 font-bold leading-none mt-6 w-11/12 self-center">
-        I'm a passionate Software Developer with years of experience building Web Apps and large websites.
+        I&apos;m a passionate Software Developer with years of experience building Web Apps and large websites.
       </h2>
       <p className="w-4/5 self-center text-comp-lighter text-center text-xl my-4 font-semibold">
         I love learning new technologies, working with colleagues and clients, and most of all building things!
