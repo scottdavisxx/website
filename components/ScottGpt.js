bf6d803158d6a1203710a9afa00f7687406bfe1c
Scott Davis <scottdavisxx@gmail.com>
2023-09-27 21:28:36 -0600

    add basic styles to component

--- components/ScottGpt.js
+++ components/ScottGpt.js
@@ -1,55 +1,56 @@
+import OpenAI from "openai";
 import { useState } from "react";
-import { Configuration, OpenAIApi } from "openai";
 
-const configuration = new Configuration({
-  organization: "scottdavisxx",
-  apiKey: process.env.OPENAI_API_KEY,
+const openai = new OpenAI({
+  dangerouslyAllowBrowser: true,
+  organization: "org-vwkn0WTV6ysfdlgUPvEhn2i1",
+  apiKey: "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",
 });
 
-const openai = new OpenAIApi(configuration);
-
-export default function ScottGpt() {
+export default function ScottGPT() {
   const [input, setInput] = useState("");
   const [output, setOutput] = useState("");
 
   const handleSubmit = async () => {
     try {
-      const prompt = input; // You might want to customize this based on your use case
-      const { choices } = await openai.createCompletion({
-        engine: "text-davinci-003", // Specify the engine you want to use
-        prompt,
+      const prompt = input;
+      const { choices } = await openai.chat.completions.create({
+        model: "gpt-3.5-turbo",
+        messages: [
+          { role: "system", content: "You are a helpful assistant." },
+          { role: "user", content: prompt },
+        ],
         max_tokens: 150,
       });
 
-      const generatedText = choices[0]?.text || "";
+      const generatedText =
+        choices[0]?.message?.content || "No response from the assistant.";
       setOutput(generatedText);
     } catch (error) {
       console.error("Error calling ChatGPT API:", error);
+      setOutput("Error calling ChatGPT API.");
     }
   };
 
   return (
-    <div className="relative h-[400px] top-12 flex justify-center items-center bg-prim">
-      <div className="flex flex-col items-center gap-y-8">
-        <label htmlFor="gpt" className="text-6xl text-comp">
-          Ask about Scott
-        </label>
-        <input
-          id="gpt"
-          value={input}
-          onChange={(e) => setInput(e.target.value)}
-        />
-        <button
-          type="button"
-          className="w-fit mb-5 bg-comp-light text-2xl rounded-lg px-3 py-1"
-          onClick={handleSubmit}
-        >
-          Submit
-        </button>
-        <div>
-          <strong>Generated Output:</strong>
-          <p>{output}</p>
-        </div>
+    <div className="h-[500px] relative top-12 bg-prim flex flex-col justify-center items-center gap-y-6">
+      <label htmlFor="gpt" className="text-white text-8xl">
+        Ask about Scott
+      </label>
+      <input
+        id="gpt"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      />
+      <button
+        onClick={handleSubmit}
+        type="submit"
+        className="w-fit mb-5 bg-comp-light text-2xl rounded-lg px-3 py-1"
+      >
+        Submit
+      </button>
+      <div>
+        <p className="text-white text-lg italic">{output}</p>
       </div>
     </div>
   );
